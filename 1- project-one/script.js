7a297e6e4fb06871516a08be6bd91709b961d24e
hishamrehab <hishamrehab30@gmail.com>
2026-02-27 23:24:29 +0200

    Data Binding

--- 1- project-one/script.js
+++ 1- project-one/script.js
@@ -6,12 +6,16 @@ createApp({
             name: "Ahmed",
             age: 20,
             email: "ahmed@gmail.com",
+            link: "https://www.google.com",
         };
     },
     methods: {
         sayHi(time, name) {
             return "Good " + time + ", " + name;
-        }
+        },
+        // sayTest: function () {
+
+        // }
     }
 }).mount("#app-root");
 
