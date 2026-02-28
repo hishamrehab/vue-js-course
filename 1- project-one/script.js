28937e4db1c2086d139235c71d52d874dd3e6c40
hishamrehab <hishamrehab30@gmail.com>
2026-02-28 17:18:14 +0200

    Looping Data (V-For)

--- 1- project-one/script.js
+++ 1- project-one/script.js
@@ -4,14 +4,30 @@ createApp({
     data() {
         return {
          name : "Ahmed",
-         age: 25,
-        };
-    },
-    methods: {
+         students: [
+            {
+                name: "Ahmed",
+                id: 1, 
+                age: 15
+            },
+                {
+                name: "Salem",
+                id: 2, 
+                age: 16
+            },
+              {
+                name: "Ahmed",
+                id: 3, 
+                age: 17
+            },
 
-    },
-    computed: {
-     
+                {
+                name: "Mohamed",
+                id: 4, 
+                age: 20
+            },
+         ],
+        };
     }
 }).mount("#app-root");
 
