63a2ee9eb05f1ad9c711c0d6f83c41489ff246dd
hishamrehab <hishamrehab30@gmail.com>
2026-02-28 16:29:57 +0200

    If Conditions & V-Show

--- 1- project-one/script.js
+++ 1- project-one/script.js
@@ -3,19 +3,15 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-         myFont: 12
+         name : "Ahmed",
+         age: 25,
         };
     },
     methods: {
 
     },
     computed: {
-        numOneChange() {
-            return this.numberOne * 2;
-        },
-        numTwoChange() {
-            return this.numberTwo + 10;
-        },
+     
     }
 }).mount("#app-root");
 
