4ad846c5b8cdfa92dee92aa2d2b2b3c3f70fa31f
hishamrehab <hishamrehab30@gmail.com>
2026-03-01 03:31:45 +0200

    Components

--- 1- project-one/script.js
+++ 1- project-one/script.js
@@ -1,16 +1,47 @@
 const { createApp } = Vue;
 
+
+const data = {
+    age: 29,
+    name : "Ahmed",
+}
+
+
+
+const my_component = {
+    template: `<h2>Hello form my forst component.</h2>
+     <h3>My Name is {{name}} </h3>
+    <h3>My Age is {{age}} </h3>
+    <button @click="changeAge">click me</button>
+` , 
+data() {
+    return data
+},
+methoods: {
+    changeAge() {
+        this.age++;
+    }
+}
+};
+
 const vue_1 = createApp({
     data() {
-      return {
-        age: 10
-      };
+       return  {}
     },
-    methods: {
-        checkRefs() {
-           this.age = this.$refs.myInput.value;
-        }
+    components : {
+        my_component,
     }
-}).mount("#app-root");
+}).mount("#app_root");
+
+
+
+const vue_2 = createApp({
+    data() {
+      return {} 
+    },
+    components : {
+        my_component,
+    }
+}).mount("#app_root_2");
 
 
