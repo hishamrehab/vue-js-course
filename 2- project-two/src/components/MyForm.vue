3fa85107e5e41480f27f076bd2f09b659431b71d
hishamrehab <hishamrehab30@gmail.com>
2026-03-05 06:56:02 +0200

    V-Model Part

---
+++ 2- project-two/src/components/MyForm.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="my-form">
+        <form action="" @submit.prevent >
+         <div>
+            <label for="">First Name</label>
+            <input type="text" v-model="firstName" />
+            <p>Result is {{ student.firstName }}</p>
+         </div>
+         <hr />
+
+           <div>
+            <label for="">Last Name</label>
+            <input type="text" v-model="lastName" />
+             <p>Result is {{ student.lastName }}</p>
+         </div>
+    <hr />
+           <div>
+            <label for="">age</label>
+            <input type="number"  v-model="age" />
+               <p>Result is {{student.age }}</p>
+         </div>
+    <hr />
+           <div>
+            <label for="">Email</label>
+            <input type="email" />
+                  <p>Result is {{ student.email }}</p>
+         </div>
+    <hr />
+         <div class="actions">
+            <input type="submit" value="Submit"  v-model="email" />
+         </div>
+
+        </form>
+    </div>
+</template>
+
+
+<script>
+export  default {
+    name : "MyForm",
+    data() {
+        return {
+            student: {
+                firstName: "",
+                lastName: "",
+                age: "",
+                email:""
+            }
+        };
+    }
+}
+</script>
--- 2- project-two/src/views/MyMain.vue
+++ 2- project-two/src/views/MyMain.vue
@@ -2,67 +2,27 @@
   <div
     class="my-main"
   >
-    <h1>
+    <h2>
       This is  my main page
-    </h1>
-   <keep-alive>
-        <my-comp :name="name" v-if="isCompTwo">
-        <template #slotHeader>
-          <h3>This is slot header one</h3>
-        </template>
-      <template  #slotInputs>
-      <div>
-        <input type="text" placeholder="Enter your name"/>
-        <input type="password" placeholder="Enter your password"/>
-      </div>
-      </template>
-            <template  #slotActions>
-      <div>
-        <input type="submit" value="submit"/>
+    </h2>
+    <MyForm />
+    
 
-      </div>
-      </template>
-    </my-comp>
-   </keep-alive>
     </div>
-     
-    <div>
-    <keep-alive>
-    <mycomp-two name="Mohamed" v-if="isCompTwo">
-            <template #slotHeader>
-            <h3>This is slot heade two</h3>
-          </template>
-        <template  #slotInputs>
-        <div>
-          <input type="text" placeholder="Enter your name"/>
-          <input type="password" placeholder="Enter your password"/>
-        </div>
-        </template>
-              <template  #slotActions>
-        <div>
-          <input type="submit" value="submit"/>
-        </div>
-        </template>
-        </mycomp-two>
-        </keep-alive>
-    </div>
-    
-    
-   <button @click="isCompTwo = !isCompTwo">Toggle Data</button>
-</template> 
+
+  </template> 
 
 
 <script>
 
-import MyComp from '@/components/MyComponent.vue';
-import MycompTwo from "@/components/MyComponent.vue"
+
+import MyForm from '@/components/MyForm.vue';
 
 
  export default {
   name : "MyMain",
   components: {
-    MyComp,
-    MycompTwo
+    MyForm
   },
   data() {
     return  {
