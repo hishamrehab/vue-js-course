a7ef3f517796efe5f47b97270ed846e4faccec11
hishamrehab <hishamrehab30@gmail.com>
2026-03-06 04:05:00 +0200

    Http Requests

--- 2- project-two/src/views/MyMain.vue
+++ 2- project-two/src/views/MyMain.vue
@@ -5,6 +5,24 @@
     <h2>
       This is  my main page
     </h2>
+    <div class="products">
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <p>title: {{ product.title }} </p>
+          <p>brand: {{ product.brand }} </p>
+          <p>category: {{ product.category }} </p>
+          <p>description: {{ product.description }} </p>
+          <p>Brande: {{ product.description }} </p>
+          <p>price: {{ product.price }} </p>
+          <p>rating: {{ product.rating }} </p>
+          <p>stock: {{ product.stock }} </p>
+          <img  :src="product.thumbnail" width="100" alt="" />
+          <hr />
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
      <MyForm />
     </div>
 
@@ -24,10 +42,19 @@ import MyForm from '@/components/MyForm.vue';
   data() {
     return  {
       name: "Ahmed",
-      isCompTwo : false
+      products: []
     }
   },
-
+  methods: {
+   async getProducts() {
+       await fetch("https://dummyjson.com/products")
+       .then(res => res.json())
+       .then((data) => (this.products = data.products));
+    }
+  },
+ async mounted() {
+   await this.getProducts();
+  },
  }
 </script>
 
